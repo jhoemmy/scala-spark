0ae02c5384f4748e39345d91cffef314c7ba0a25
Jomy G <jhoemmy@gmail.com>
2016-01-05 18:41:59 -0500

    First Commit for the scala-spark test.

---
+++ scala-spark/src/main/scala/org/scala/spark/scalaspark/sparktest.scala
@@ -0,0 +1,20 @@
+package org.scala.spark.scalaspark
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+object sparktest {
+  def main(args: Array[String]) = {
+
+    //Start the Spark context
+    val conf = new SparkConf()
+      .setAppName("sparktest")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+    
+    //Read some example file to a test RDD
+    val fileholder = sc.textFile("c://study/spark/spark1.6/README.md")
+    val numHadoop = fileholder.filter(line => line.contains("Hadoop")).count()
+    val numSpark = fileholder.filter(line => line.contains("Spark")).count()
+    println(s"Number of Hadoop in the file : $numHadoop and Number of Sparks : $numSpark")
+  }
+}
